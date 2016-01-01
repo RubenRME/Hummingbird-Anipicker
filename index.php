2ec8ea94076af3731b562f9c225d7501f3f8d1e4
Ruben Engelbrecht <me@rubenengelbrecht.com>
2016-01-01 17:27:27 +0100

    Removed one line If/else

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 			<h1 class="center huge text">Hummingbird Anipicker!</h1>
 			<form class="align center form" action="index.php" method="get">
 				<input type="text" name="user" value="<?php echo $_GET['user'] ?>" autofocus  placeholder="Type your HB username and press Enter!" />
-				<input type="checkbox" name="force-to-watch" value="true" <?php if($_GET['force-to-watch'] == true) {echo "checked";}else{echo "";} ?> /><label>Force to Watch</label>
+				<input type="checkbox" name="force-to-watch" value="true" <?php echo $_GET['force-to-watch'] ? "checked" : "" ?> /><label>Force to Watch</label>
 			</form>
 			<br />
 		</div>
 		<div class="body">
 			<?php 
-				if(getAPI($_GET['user']) == FALSE) {
+				if(getAPI($_GET['user']) == false) {
 					echo '<h1 class="big text center">No Username Filled in</h1>';
 				} else {
 					getRandomAnime();
