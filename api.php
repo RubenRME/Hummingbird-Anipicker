ca4849606e7351b39412fd9e4111fcbdc6680412
Daniel Taylor <kingorgg@gmail.com>
2016-01-05 23:09:52 +0000

    Fixed: Force to Watch checkbox

--- api.php
+++ api.php
@@ -1,55 +1,60 @@
 <?php
 
 // Get rid of stupid errors when GET is not set
-if(!isset($_GET['user'])) { $_GET['user'] = ""; }
-if(!isset($_GET['force-to-watch'])) { $_GET['force-to-watch'] = true; }
+if (!isset($_GET['user'])) {
+    $_GET['user'] = '';
+}
+if (!isset($_GET['force-to-watch'])) {
+    $_GET['force-to-watch'] = false;
+}
 
-/* 
+/*
  * Check if USERNAME if valid
  * If not: return false
  * If yes: return true
  */
-function getAPI($username) {
-	$user = "https://hummingbird.me/api/v1/users/" . $_GET['user'];
-	
-	$library = @file_get_contents($user);
-		
-	if($library == false) {
-		return false;
-	} else {
-		return true;
-	}
+function getAPI($username)
+{
+    $user = 'https://hummingbird.me/api/v1/users/'.$_GET['user'];
+
+    $library = @file_get_contents($user);
+
+    if ($library == false) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
-/* 
+/*
  * Get a random anime from USERNAME's plan-to-watch
  */
-function getRandomAnime() {
+function getRandomAnime()
+{
+    $currentlywatching = json_decode(file_get_contents('https://hummingbird.me/api/v1/users/'.$_GET['user'].'/library?status=currently-watching'), true);
+    $plantowatch = json_decode(file_get_contents('https://hummingbird.me/api/v1/users/'.$_GET['user'].'/library?status=plan-to-watch'), true);
 
-	$currentlywatching = json_decode(file_get_contents("https://hummingbird.me/api/v1/users/" . $_GET['user'] . "/library?status=currently-watching"), true);
-	$plantowatch = json_decode(file_get_contents("https://hummingbird.me/api/v1/users/" . $_GET['user'] . "/library?status=plan-to-watch"), true);
-	
-	//Check if force-to-watch is on
-	if($_GET['force-to-watch'] == true) {
-		// Check if currently-watching containst more than 5 items, if so a random anime from currently-watching should be selected
-		if(count($currentlywatching) > 5) {
-			$selection = $currentlywatching;
-		} else {
-			$selection = $plantowatch;
-		}
-	} else {
-		$selection = $plantowatch;
-	}
-	
-	$random = $selection[array_rand($selection)];
-	
-	// Loop to make sure anime is already airing
-	while($random['anime']['status'] == "Not Yet Aired") {
-		$random = $selection[array_rand($selection)];
-	}
-	$random_title = $random['anime']['title'];
-	$random_alt_title = $random['anime']['alternate_title'];
-	$random_link = $random['anime']['url'];
-	echo "<h1 class='big text center'><a href='$random_link' target='_blank'>$random_title</a></h1>";
-	echo "<h2 class='medium text center'>$random_alt_title</h2>";
+    //Check if force-to-watch is on
+    if ($_GET['force-to-watch'] == true) {
+        // Check if currently-watching containst more than 5 items, if so a random anime from currently-watching should be selected
+        if (count($currentlywatching) > 5) {
+            $selection = $currentlywatching;
+        } else {
+            $selection = $plantowatch;
+        }
+    } else {
+        $selection = $plantowatch;
+    }
+
+    $random = $selection[array_rand($selection)];
+
+    // Loop to make sure anime is already airing
+    while ($random['anime']['status'] == 'Not Yet Aired') {
+        $random = $selection[array_rand($selection)];
+    }
+    $random_title = $random['anime']['title'];
+    $random_alt_title = $random['anime']['alternate_title'];
+    $random_link = $random['anime']['url'];
+    echo "<h1 class='big text center'><a href='$random_link' target='_blank'>$random_title</a></h1>";
+    echo "<h2 class='medium text center'>$random_alt_title</h2>";
 }
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require_once('api.php'); ?>
+<?php require_once 'api.php'; ?>
 <!DOCTPYE html>
 <html>
 	<head>
@@ -11,23 +11,23 @@
 			<h1 class="center huge text">Hummingbird Anipicker!</h1>
 			<form class="align center form" action="index.php" method="get">
 				<input type="text" name="user" value="<?php echo $_GET['user'] ?>" autofocus  placeholder="Type your HB username and press Enter!" />
-				<input type="checkbox" name="force-to-watch" value="true" <?php echo $_GET['force-to-watch'] ? "checked" : "" ?> /><label>Force to Watch</label>
+				<input type="checkbox" name="force-to-watch" value="true" <?php echo $_GET['force-to-watch'] ? 'checked' : '' ?> /><label>Force to Watch</label>
 			</form>
 			<br />
 		</div>
 		<div class="body">
-			<?php 
-				if(getAPI($_GET['user']) == false) {
-					if(!isset($_GET['user'])) {
-						echo '<h1 class="big text center">No Username Filled in</h1>';
-					}else{
-						echo '<h1 class="big text center">Unexisting username</h1>';
-						echo '<h2 class="medium text center">Hummingbird responds with error 404</h2>';
-					}
-				} else {
-					getRandomAnime();
-				}
-			?>
+			<?php
+                if (getAPI($_GET['user']) == false) {
+                    if (!isset($_GET['user'])) {
+                        echo '<h1 class="big text center">No Username Filled in</h1>';
+                    } else {
+                        echo '<h1 class="big text center">Unexisting username</h1>';
+                        echo '<h2 class="medium text center">Hummingbird responds with error 404</h2>';
+                    }
+                } else {
+                    getRandomAnime();
+                }
+            ?>
 		</div>
 	</body>
 </html>
